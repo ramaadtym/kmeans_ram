e3e18c18e432a3abc1b292a3d63bd4ff6fd98fa7
ramaadtym <ramaadtym@gmail.com>
2018-04-02 14:27:46 +0700

    Update nyari centroid random

--- kmeans_ram.py
+++ kmeans_ram.py
@@ -7,31 +7,10 @@ import random
 arr = []
 x0 = []
 y0 =[]
+centroid = []
 x1 = []
 y1 = []
 attr = 0
-# with open ("TrainsetTugas2.txt","r") as brks:
-#     data = brks.readlines()
-#     for x in data:
-#         brs = x.split()
-#         x0.append([float(brs[0])])
-#         y0.append([float(brs[1])])
-#     attr += 1
-#
-# for i in range(len(x0)):
-#     x0.append(np.array(x0[i]))
-#     y0.append(np.array(y0[i]))
-#
-# print("Atribut X=",x0)
-# print("Atribut Y=",y0)
-#
-# graf = plt.figure()
-# graf.canvas.set_window_title('Visualisasi Data Training')
-# data = graf.add_subplot(111)
-# data.set_xlabel('Attr 1')
-# data.set_ylabel('Attr 2')
-# data.scatter(x0,y0, c='black', marker='+')
-# plt.show()
 
 class kmeans:
     def __init__(self, file):
@@ -45,35 +24,35 @@ with contextlib.closing(kmeans("TrainsetTugas2.txt"))as berkas:
     data = berkas.brks.readlines()
     for x in data:
         brs = x.split()
-        x0.append([float(brs[0]),float(brs[1])])
-        # y0.append([float(brs[1])])
+        centroid.append([float(brs[0]),float(brs[1])])
+        x0.append([float(brs[0])])
+        y0.append([float(brs[1])])
     attr += 1
 
-with contextlib.closing(kmeans("TestsetTugas2.txt"))as berkas:
-    data = berkas.brks.readlines()
-    for x in data:
-        brs = x.split()
-        x1.append([float(brs[0])])
-        y1.append([float(brs[1])])
-    attr += 1
+# with contextlib.closing(kmeans("TestsetTugas2.txt"))as berkas:
+#     data = berkas.brks.readlines()
+#     for x in data:
+#         brs = x.split()
+#         x1.append([float(brs[0])])
+#         y1.append([float(brs[1])])
+#     attr += 1
 
 for i in range(len(x0)):
     x0.append(np.array(x0[i]))
-    # y0.append(np.array(y0[i]))
+    y0.append(np.array(y0[i]))
 
-print(random.choice(x0))
+print("Centroid 1 = ", random.choice(centroid))
+print("Centroid 2 = ", random.choice(centroid))
+print("Atribut X=",x0)
+print("Atribut Y=",y0)
 
+graf = plt.figure()
+graf.canvas.set_window_title('Visualisasi Data Training')
+data = graf.add_subplot(111)
+data.set_xlabel('Attr 1')
+data.set_ylabel('Attr 2')
+data.scatter(x0,y0, c='black', marker='+')
+plt.show()
 
-# print("Atribut X=",x0)
-# print("Atribut Y=",y0)
-#
-# graf = plt.figure()
-# graf.canvas.set_window_title('Visualisasi Data Training')
-# data = graf.add_subplot(111)
-# data.set_xlabel('Attr 1')
-# data.set_ylabel('Attr 2')
-# data.scatter(x0,y0, c='black', marker='+')
-# plt.show()
-#
-# berkas.brks.close()
+berkas.brks.close()
 
