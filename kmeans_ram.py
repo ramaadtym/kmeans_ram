133cb60befb5ece122845f6d9ff71dfbc9dadd7b
ramaadtym <ramaadtym@gmail.com>
2018-04-05 09:11:19 +0700

    Update nyari SSE

--- kmeans_ram.py
+++ kmeans_ram.py
@@ -11,8 +11,9 @@ titik = []
 centroid = []
 minx = []
 miny = []
-x1 = []
-y1 = []
+jarak = []
+sse = []
+jSSE = []
 attr = 0
 
 
@@ -26,6 +27,7 @@ class kmeans:
             self.brks = None
 
 
+# with contextlib.closing(kmeans("TrainsetTugas2.txt"))as berkas:
 with contextlib.closing(kmeans("datakecil.txt"))as berkas:
     data = berkas.brks.readlines()
     for x in data:
@@ -35,22 +37,6 @@ with contextlib.closing(kmeans("datakecil.txt"))as berkas:
         y0.append([float(brs[1])])
     attr += 1
     berkas.brks.close()
-
-# with contextlib.closing(kmeans("TestsetTugas2.txt"))as berkas:
-#     data = berkas.brks.readlines()
-#     for x in data:
-#         brs = x.split()
-#         x1.append([float(brs[0])])
-#         y1.append([float(brs[1])])
-#     attr += 1
-
-#asumsi k = 2
-# c1 = random.choice(titik)
-# c2 = random.choice(titik)
-# print("Centroid 1 = ", c1)
-# print("Centroid 2 = ", c2)
-
-# k = random.randint(2,5)
 k = 2
 print(k)
 
@@ -58,30 +44,24 @@ print(k)
 for t in range(k):
     getCentroid = random.choice(titik)
     centroid.append(getCentroid)
-    print("Centroid ke-",t+1,getCentroid)
-print(centroid)
-# print(centroid[0][0])
-# print(centroid[0][1])
-# print(centroid[1][0])
+    # print("Centroid ke-",t+1,getCentroid)
+g = 0
+i = 0
+
 for i in range(len(titik)):
     a = titik[i][0]
     b = titik[i][1]
-    jarak = (a - centroid[0][0])**2 +  (b - centroid[0][1])**2
-    jarak2 = (a - centroid[1][0])**2 +  (b - centroid[1][1])**2
-    if jarak <  jarak2:
-        kelas = "Kelas 1"
-    else:
-        kelas = "kelas 2"
-    print("Data ke-",i,"|",jarak,"|",jarak2," | Titik Terdekat ", min(jarak, jarak2),"|",kelas)
-# print("nilai min",min(jrk))
-
-
-    # for i in range(len(titik)):
-    #     a = titik[i][0]
-    #     b = titik[i][1]
-    #     jrkC1 = (a - centroid[0])**2 +  (b - centroid[1])**2
-    #     print("Jarak titik ke centroid ke kelas",t+1,"= ", jrkC1)
-
+    for g in range(k):
+        jarak.append((a - centroid[g][0]) ** 2 + (b - centroid[g][1]) ** 2)
+        g += 1
+    sse.append(min(jarak[i],jarak[i+1]))
+#     print(sse)
+#     print(sum(sse))
+#     jSSE.append(sum(sse))
+# print(jSSE)
+# print(min(jSSE))
+    # print(min(sum(sse)))
+# print("Nilai Min SSE dari ",j+1," Percobaan",min(jSSE))
 
 # graf = plt.figure()
 # graf.canvas.set_window_title('Visualisasi Data Training')
